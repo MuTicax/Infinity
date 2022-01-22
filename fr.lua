1ce607e1121a439530f7424db0e46f7e0831ae2e
MuTicax <muticax@gmail.com>
2022-01-22 23:06:28 +0100

    New

---
+++ fr.lua
@@ -0,0 +1,45 @@
+Translation = {
+    -- IDENTITY
+    ['fr'] = {
+        ['select_character'] = 'My Characters',
+        ['select_character_desc'] = '~b~Choose a Character.',
+        ['new_character'] = '~b~Create new Character',
+        ['new_character_desc'] = 'Create a new Character.',
+        ['slots_full'] = 'You do not have enough Char Slots!',
+
+        ['select_title'] = 'Register',
+        ['last_position'] = '~b~→ ~s~Last Position',
+        ['position_desc'] = 'Click to spawn at ~b~',
+        ['position_desc_2'] = '~s~.',    
+        
+        ['register_title'] = 'Create character',
+        ['register_title_desc'] = 'Complete your ID-Card!',
+        ['gender_m'] = ' male ',
+        ['gender_f'] = ' female ',
+        ['name'] = 'First name',
+        ['lastname'] = 'Last name',
+        ['dob'] = 'Date of birth',
+        ['height'] = 'Height',
+        ['sex'] = 'Gender',
+        ['confirm'] = '~b~Enter the island',
+        ['confirm_desc'] = 'Everything correct?',
+        ['insert_name'] = 'Enter first name',
+        ['insert_lastname'] = 'Enter last name',
+        ['insert_dob'] = 'Enter date of birth (example: 01.01.2000)',
+        ['insert_height'] = 'Insert height (140-200)',
+        ['height_unit'] = 'cm',
+        ['register_error'] = '~r~At least one field is not correct!',
+
+        ['ped_models'] = 'Ped Models',
+        ['default_ped'] = 'Default Ped',
+        ['pedmode_no_perms'] = '~r~You can not do this!',
+
+        ['enter_char'] = 'Enter',
+        ['delete_char'] = '~r~Delete character',
+        ['delete_char_conirm'] = '~r~Delete character confirm',
+
+        ['giveperm_wrong_usage'] = '~r~Wrong usage! ~w~/giveperm [Player-ID] [charamount/pedmode] [Value]',
+        ['giveperm_success'] = '~g~Successfully added permission',
+        ['giveperm_error'] = '~r~Player is not online!',
+    }
+}
